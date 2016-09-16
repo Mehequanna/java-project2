54840889bd1423b574c28c13642b9d9adeef2a13
Stephen Emery <emery.stephen@gmail.com>
2016-09-16 10:31:44 -0700

    Add routing and template for a single word.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -35,5 +35,13 @@ public class App {
       model.put("template", "templates/word-success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/words/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Word userWord = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", userWord);
+      model.put("template", "templates/word.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
