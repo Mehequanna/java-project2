7cdad926e078ddbe80eae402a76b29d8afc055a5
Stephen Emery <emery.stephen@gmail.com>
2016-09-16 08:25:45 -0700

    Add inital files and start word class.

---
+++ README.md
@@ -0,0 +1,23 @@
+## Project Name
+Make your own dictionary Dictionary
+
+## Features
+This app will allow you to use a browser to add words and definitions and see them.
+
+##Technologies
+Java, JUnit, Gradle, Spark
+
+## Usage
+To use the code, you can clone the repository at: [https://github.com/mehequanna/java-project2](https://github.com/mehequanna/java-project2).
+If this were a program:
+* $ git clone https://github.com/mehequanna/java-project2
+* $ cd java-project2
+* $ gradle run
+* Go to localhost:4567 in your browser
+
+## Author/s
+Stephen Emery
+
+##License
+This work can be used under the MIT License.
+Copyright (c) 2016 Stephen Emery
---
+++ src/main/java/Word.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Word {
+  private String mTheWord;
+  private static List<Word> instances = new ArrayList<Word>();
+  private int mId;
+
+  public Word(String theWord) {
+    mTheWord = theWord;
+    mId = instances.size();
+    instances.add(this);
+  }
+
+  public String getTheWord () {
+    return mTheWord;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public static Word find(int id) {
+    try {
+      return instances.get(id);
+    } catch (IndexOutOfBoundsException exception) {
+      return null;
+    }
+  }
+
+}
